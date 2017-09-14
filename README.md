4a27c66f656e6ab3ea6fe374f6f0c62d2889e403
Jorge Herrera-Camara <jorgeivan000@gmail.com>
2017-09-14 16:36:42 -0500

    Added html and assets

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# cmcu_wheel
-Customized wheel of fortune using Phaser Roulette.js
+# Phaser Roulette Example
+
+![](http://befiveinfo.github.io/images/shared/roulette-experiment/roulette_thumbnail.png)
+
+This [Phaser JS](https://phaser.io/) script demonstartes a solution to show roulette that rotates and stops at a random position. Needle ticks with animation and sound as the roulette spins.
+
+The script itself could be rewritten into a really simple code and should be able to be integrate into any game at ease. You could use it for a simple game, a small lottery in your game, board games etc.
+
+## Online Demo
+
+Online demo is available at [this website](http://befive.info/phaser_roulette_example/).
+
+## To try this script
+
+1. Make a directory in your web server and copy the content into it.
+2. Open http:://yourserver/the_directory_of_your_choice/index.html in your browser
+- Note: You can still try the demo by simply double clicking index.html in your computer's desktop. But you will not hear any sound because of CORS issue (please [see here](http://www.html5gamedevs.com/topic/6459-newbie-struggling-with-cors-issues/) for more info).
+
+# Application
+Games made with a roulette system based on this demo script can be found at:
+- [Fuji San Roulette (OPEN SOURCE)](https://github.com/BeFiveINFO/fuji-san-roulette)
+  - ![](http://befiveinfo.github.io/images/shared/fujisan-roulette/thumbnail.png)
+  - This games is made with the Phaser Roulette Example script. Published as an open source.
+- [Dealer Ginjirou (Closed Source)](http://play.befive.info/dealer-ginjirou/)
+  - ![](http://befiveinfo.github.io/images/shared/roulette-experiment/dealer_ginjirou_gamescreen_thumbnail.png)
+  - The game uses a single PNG with transparent for the spin wheel discs.
+
+# License
+MIT
---
+++ js/script.js
@@ -0,0 +1,115 @@
+/**
+ * Licensed MIT.
+ *
+ * Sorry about the rudimentary scribble code below.
+ *
+ * The code in the repo might be drastically changed in the future without any notices.
+ *
+ * But
+ *
+ * The following code at least works and demonstrates as written in the read me.
+ */
+
+var Game = new Phaser.Game(600,600,Phaser.CANVAS,'gameContainer', { preload: preload, create: create, update: update}, true );
+
+var tickSound;
+var currentPocketDisplay;
+
+function preload () {
+	loadingLabel = Game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#fff'});
+	Game.load.image('pocket_black', 'images/pocket_black.png');
+	Game.load.image('pocket_red', 'images/pocket_red.png');
+	Game.load.image('needle', 'images/needle.png');
+	Game.load.image('elmo', 'images/elmo.png');
+	Game.load.audio('tick', 'sound/waka.wav');
+}
+
+function create () {
+	loadingLabel.destroy();
+	Game.time.advancedTiming = true;
+	Game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+	/**
+	 * Required Instances
+	 */
+	tickSound = Game.add.audio('tick');
+	tickSound.allowMultiple = true;
+
+	/**
+	 * For debugging
+	 */
+	currentPocketDisplay = Game.add.text(0, 0, '0', {font:"bold 128px Arial",fill: "#fff",boundsAlignH: "center",boundsAlignV: "middle"});
+	currentPocketDisplay.setTextBounds(0, 200, 595, 230);
+
+	/**
+	 * Events
+	 */
+	PCB.event.add('rouletteInitialized',function() {
+	});
+	PCB.event.add('reelSpinStarted',function() {
+		tickSound.play('',0,1,true);
+	});
+	PCB.event.add('reelCountDownStarted',function() {
+		tickSound.stop();
+	});
+	PCB.event.add('currentPocketChanged',function() {
+
+	});
+	PCB.event.add('currentPocketChangedAfterCountDown',function() {
+		tickSound.play('',0,1,false);
+		outputCurrentStop();
+	});
+	PCB.event.add('currentPocketChangedBeforeCountDown',function() {
+		outputCurrentStop();
+	});
+	PCB.event.add('pocketChangedAtLastMoment',function() {
+		tickSound.play('',0,1,false);
+	});
+	PCB.event.add('onCompleteSpinReel',function() {
+		outputCurrentStop();
+	});
+
+	/**
+	 * Init roulette
+	 */
+	rouletteModule.init('main');
+
+	/**
+	 * Key Events
+	 */
+	var key1 = Game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	key1.onDown.add(function() { rouletteModule.spinRoulette('main');} , this);
+	// nudge the disc to left
+	var key2 = Game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+	key2.onDown.add(function() {
+		var $_rouletteUnit = rouletteModule.rouletteUnits.main;
+		$_rouletteUnit.disc.angle--;
+		$_rouletteUnit.needle.angle = 0;
+		rouletteModule._getCurrentStopID('main',true);
+		outputCurrentStop();
+	}, this);
+	// nudge the disc to right
+	var key3 = Game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+	key3.onDown.add(function() {
+		var $_rouletteUnit = rouletteModule.rouletteUnits.main;
+		$_rouletteUnit.disc.angle++;
+		$_rouletteUnit.needle.angle = 0;
+		rouletteModule._getCurrentStopID('main',true);
+		outputCurrentStop();
+	}, this);
+
+	// mouse input
+	var _click = (window.ontouchstart === undefined)? 'click' : 'touchstart';
+	Game.canvas.addEventListener(_click, function () {
+		rouletteModule.spinRoulette('main');
+	});
+}
+
+function update () {
+}
+
+function outputCurrentStop(){
+	var $_targetUnit = rouletteModule.rouletteUnits.main;
+	var _pocketIndex = $_targetUnit.currentStop;
+	currentPocketDisplay.setText($_targetUnit.pocketBetStrip[_pocketIndex]);
+}
